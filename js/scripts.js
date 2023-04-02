bb28a4667c064f9a3f261bc8d2eb8b4f7a44204a
JYLim89 <ljypower1@gmail.com>
2023-04-02 21:11:05 +0900

    23.04.02

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/*!
+* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+
+// https://www.youtube.com/watch?v=tmWvwp3rpso
+document.addEventListener("click",function (e){
+    if(e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+    }
+    })  
+
