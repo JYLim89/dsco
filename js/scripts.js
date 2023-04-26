4ed4d504073f06c26491c45fdeb208d5de7b98ce
JYLim89 <ljypower1@gmail.com>
2023-04-26 11:40:22 +0900

    23.04.26

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,28 @@
 
 // https://www.youtube.com/watch?v=tmWvwp3rpso
 document.addEventListener("click",function (e){
-    if(e.target.classList.contains("gallery-item")){
-        const src = e.target.getAttribute("src");
-        document.querySelector(".modal-img").src = src;
-        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
-        myModal.show();
-    }
-    })  
+	if(e.target.classList.contains("gallery-item")){
+		const src = e.target.getAttribute("src");
+		document.querySelector(".modal-img").src = src;
+		const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+		myModal.show();
+	}
+	})  
 
+	function mailSubmit() {
+		const contactForm = document.getElementById('contactForm');
+		const name = contactForm.name.value;
+		const email = contactForm.email.value;
+		const phone = contactForm.phone.value;
+		const message = contactForm.message.value;
+	
+		if (!name || !email || !phone || !message) {
+			alert('Input all information');
+		} else if (!/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.test(email)) {
+			alert('Input valid email address');
+		} else {
+			contactForm.submit();
+			alert('submit successfully!');
+		}
+	}
+	
